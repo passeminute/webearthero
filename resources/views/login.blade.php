3d40641d44b7a7bf44fd4161d50f1db086f1dd9e
passeminute <passeminute@gmail.com>
2022-03-06 00:45:30 +0700

    alur login di malam minggu

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,21 +3,22 @@
 <div class="container custom-login">
     <div class="row justify-content-center">
         <div class="col-sm-4">
-            <form action="/action_page.php">
+            <form action="/login" method="POST">
                 <div class="mb-3 mt-3">
+                    @csrf
                     <label for="email" class="form-label">Email:</label>
                     <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
                 </div>
                 <div class="mb-3">
                     <label for="pwd" class="form-label">Password:</label>
-                    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+                    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="password">
                 </div>
                 <div class="form-check mb-3">
                     <label class="form-check-label">
                     <input class="form-check-input" type="checkbox" name="remember"> Remember me
                     </label>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Login</button>
             </form>
         </div>
     </div>
